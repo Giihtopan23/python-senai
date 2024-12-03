d1b7bab1eecadcb306da167f1ffa8ba0f57c29bd
Giihtopan23 <giovanna19leandra@gmail.com>
2024-12-03 09:54:47 -0300

    aula 02

---
+++ AULA 2/aula o2.py
@@ -0,0 +1,12 @@
+print ('Olá,bom dia')
+print('que dia mais gostoso para dormir com chuva')
+nome_usuario="Giovanna" # str = string
+idade_usuario= 23 # int= integer 
+altura_usuario= 1.65 # float = ponto flutuante
+maio_idade= True # bool= boolear 
+print("seja bem-vindo",nome_usuario)
+print('seja bem-vindo', idade_usuario)
+print('seja bem-vindo',altura_usuario)
+print('seja bem-vindo', maio_idade)
+
+
